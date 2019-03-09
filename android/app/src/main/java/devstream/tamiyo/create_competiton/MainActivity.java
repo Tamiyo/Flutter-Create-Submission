c710ec26ae63c86cc7dbd39c3c4a53e37c295844
Matthew McMillian <www.matthewgmcmillian@gmail.com>
2019-03-08 20:10:21 -0600

    Initial Commit

---
+++ android/app/src/main/java/devstream/tamiyo/create_competiton/MainActivity.java
@@ -0,0 +1,13 @@
+package devstream.tamiyo.create_competiton;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
